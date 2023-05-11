8c801d9946ddc874eddf50d4156f03befd73a131
limseungchae <tmdco12@naver.com>
2023-05-11 14:48:24 +0900

    mvc 프로젝트 - tiles 설정

---
+++ src/main/java/seung/springboot/semiprojectv5/configuration/TilesConfig.java
@@ -0,0 +1,28 @@
+package seung.springboot.semiprojectv5.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesView;
+
+@Configuration
+public class TilesConfig {
+
+    @Bean
+    public UrlBasedViewResolver tilesViewResolver() {
+        UrlBasedViewResolver tilesViewResolver = new UrlBasedViewResolver();
+        tilesViewResolver.setViewClass( TilesView.class );
+
+        return tilesViewResolver;
+    }
+
+    @Bean
+    TilesConfigurer tilesConfigurer() {
+        TilesConfigurer tilesConfigurer = new TilesConfigurer();
+        String[] defs = { "classpath:tiles.xml" };
+        tilesConfigurer.setDefinitions( defs );
+
+        return tilesConfigurer;
+    }
+}
